a281906cea659464930a40a3c61a6dc9365a6907
SuperJim <526554176@qq.com>
2022-12-17 09:59:27 +0900

    Add project files.

---
+++ MySubModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace JimAIExecutionMod
+{
+    public class MySubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
+                     new TextObject("Message", null),
+                     9990,
+                     () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); },
+                     () => { return (false, null); }));
+
+        }
+    }
+}
