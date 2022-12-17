4b32216f724855f0e8713de47d18751ad0845df2
SuperJim <526554176@qq.com>
2022-12-17 14:56:10 +0900

    Added a quick notification on execution. You can even uncomment the commented line in the code to add an execution scene, haha!

--- JimAIExecutionBehavior.cs
+++ JimAIExecutionBehavior.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.LogEntries;
+using TaleWorlds.CampaignSystem.SceneInformationPopupTypes;
+using TaleWorlds.Core;
 using TaleWorlds.Library;
 
 namespace JimAIExecutionMod
@@ -21,6 +24,9 @@ namespace JimAIExecutionMod
                         if (!(prisoner == Hero.MainHero))
                         {
                             KillCharacterAction.ApplyByExecution(prisoner, captureParty.LeaderHero, true);
+                            //// The following line will create an execution scene, just like when player(the main hero) is executing a hero.
+                            //MBInformationManager.ShowSceneNotification(HeroExecutionSceneNotificationData.CreateForInformingPlayer(captureParty.LeaderHero, prisoner, SceneNotificationData.RelevantContextType.Map));
+                            MBInformationManager.AddQuickInformation(new CharacterKilledLogEntry(prisoner, captureParty.LeaderHero, KillCharacterAction.KillCharacterActionDetail.Executed).GetNotificationText(), 0, prisoner.CharacterObject, "event:/ui/notification/death");
                         }
                     }
                     catch (Exception ex)
