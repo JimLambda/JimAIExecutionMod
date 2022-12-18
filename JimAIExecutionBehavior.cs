d72c5a60f7034084a22ee2e2e24deedb11d0f34c
SuperJim <526554176@qq.com>
2022-12-18 15:17:41 +0900

    Added configuration options! Now the probability of execution can be adjusted!

--- JimAIExecutionBehavior.cs
+++ JimAIExecutionBehavior.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
 using TaleWorlds.CampaignSystem.LogEntries;
 using TaleWorlds.CampaignSystem.SceneInformationPopupTypes;
 using TaleWorlds.Core;
@@ -14,6 +15,8 @@ namespace JimAIExecutionMod
 {
     internal class JimAIExecutionBehavior : CampaignBehaviorBase
     {
+        Random random = new Random();
+
         public override void RegisterEvents()
         {
             CampaignEvents.HeroPrisonerTaken.AddNonSerializedListener(this, new Action<TaleWorlds.CampaignSystem.Party.PartyBase, Hero>(
@@ -21,12 +24,43 @@ namespace JimAIExecutionMod
                 {
                     try
                     {
+                        ReadConfigurationFromOuterFile.SetConfigurationDataToDefault();
+                        ReadConfigurationFromOuterFile.ReadDataFromConfigurationFile();
+
                         if (!(prisoner == Hero.MainHero || captureParty.LeaderHero == Hero.MainHero))
                         {
-                            KillCharacterAction.ApplyByExecution(prisoner, captureParty.LeaderHero, true);
-                            //// The following line will create an execution scene, just like when player(the main hero) is executing a hero.
-                            //MBInformationManager.ShowSceneNotification(HeroExecutionSceneNotificationData.CreateForInformingPlayer(captureParty.LeaderHero, prisoner, SceneNotificationData.RelevantContextType.Map));
-                            MBInformationManager.AddQuickInformation(new CharacterKilledLogEntry(prisoner, captureParty.LeaderHero, KillCharacterAction.KillCharacterActionDetail.Executed).GetNotificationText(), 0, prisoner.CharacterObject, "event:/ui/notification/death");
+                            Double executionProbability = 100;
+
+                            if (ReadConfigurationFromOuterFile.doYouWantGlobalExecutionProbability)
+                            {
+                                executionProbability = ReadConfigurationFromOuterFile.globalExecutionProbability;
+                            }
+
+                            if (ReadConfigurationFromOuterFile.doYouWantRelationFactor)
+                            {
+                                int leaderRelationTowardsPrisoner = captureParty.LeaderHero.GetRelation(prisoner);
+                                executionProbability = executionProbability - leaderRelationTowardsPrisoner * ReadConfigurationFromOuterFile.relationFactor;
+                            }
+
+                            if (ReadConfigurationFromOuterFile.doYouWantTraitAffect)
+                            {
+                                int leaderMercyTraitLevel = captureParty.LeaderHero.GetTraitLevel(DefaultTraits.Mercy);
+                                int leaderHonorTraitLevel = captureParty.LeaderHero.GetTraitLevel(DefaultTraits.Honor);
+                                int leaderGenerosityTraitLevel = captureParty.LeaderHero.GetTraitLevel(DefaultTraits.Generosity);
+                                int leaderCalculatingTraitLevel = captureParty.LeaderHero.GetTraitLevel(DefaultTraits.Calculating);
+
+                                executionProbability = executionProbability - leaderMercyTraitLevel * ReadConfigurationFromOuterFile.traitLevelFactorMercy - leaderHonorTraitLevel * ReadConfigurationFromOuterFile.traitLevelFactorHonor - leaderGenerosityTraitLevel * ReadConfigurationFromOuterFile.traitLevelFactorGenerosity - leaderCalculatingTraitLevel * ReadConfigurationFromOuterFile.traitLevelFactorCalculating;
+                            }
+
+                            Double randomDoubleBetween0And100 = random.NextDouble() * 100;
+                            if (randomDoubleBetween0And100 < executionProbability)
+                            {
+                                KillCharacterAction.ApplyByExecution(prisoner, captureParty.LeaderHero, true);
+                                //// The following line will create an execution scene, just like when player(the main hero) is executing a hero.
+                                //MBInformationManager.ShowSceneNotification(HeroExecutionSceneNotificationData.CreateForInformingPlayer(captureParty.LeaderHero, prisoner, SceneNotificationData.RelevantContextType.Map));
+                                MBInformationManager.AddQuickInformation(new CharacterKilledLogEntry(prisoner, captureParty.LeaderHero, KillCharacterAction.KillCharacterActionDetail.Executed).GetNotificationText(), 0, prisoner.CharacterObject, "event:/ui/notification/death");
+                            }
+
                         }
                     }
                     catch (Exception ex)
---
+++ ReadConfigurationFromOuterFile.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JimAIExecutionMod
+{
+    internal static class ReadConfigurationFromOuterFile
+    {
+        public static Boolean doYouWantGlobalExecutionProbability = false;
+        public static Double globalExecutionProbability = 100;
+        public static Boolean doYouWantRelationFactor = false;
+        public static Double relationFactor = 0;
+        public static Boolean doYouWantTraitAffect = false;
+        public static Double traitLevelFactorMercy = 0;
+        public static Double traitLevelFactorHonor = 0;
+        public static Double traitLevelFactorGenerosity = 0;
+        public static Double traitLevelFactorCalculating = 0;
+
+
+        public static void ReadDataFromConfigurationFile()
+        {
+            string currentExecutingAssemblyLocation = Assembly.GetExecutingAssembly().Location;
+            string configurationFileDirectoryPath = new DirectoryInfo(currentExecutingAssemblyLocation).Parent.Parent.Parent.FullName;
+            string configurationFilePath = Path.Combine(configurationFileDirectoryPath, "JimAIExecutionModConfiguration.txt");
+            if (File.Exists(configurationFilePath))
+            {
+                foreach (string readLine in File.ReadLines(configurationFilePath))
+                {
+                    if (!readLine.StartsWith("#") && readLine.Length > 1)
+                    {
+                        string[] stringArray = readLine.Split('=');
+                        if (stringArray.Length == 2 && Double.TryParse(stringArray[1], out _))
+                        {
+                            int tempInteger = 0;
+                            Double tempDouble = 0;
+                            switch (stringArray[0])
+                            {
+                                case "DoYouWantGlobalExecutionProbability":
+                                    if (int.TryParse(stringArray[1], out tempInteger))
+                                    {
+                                        if (tempInteger == 1)
+                                        {
+                                            doYouWantGlobalExecutionProbability = true;
+                                        }
+                                    }
+                                    break;
+                                case "GlobalExecutionProbability":
+                                    if (Double.TryParse(stringArray[1], out tempDouble))
+                                    {
+                                        globalExecutionProbability = tempDouble;
+                                    }
+                                    break;
+                                case "DoYouWantRelationFactor":
+                                    if (int.TryParse(stringArray[1], out tempInteger))
+                                    {
+                                        if (tempInteger == 1)
+                                        {
+                                            doYouWantRelationFactor = true;
+                                        }
+                                    }
+                                    break;
+                                case "RelationFactor":
+                                    if (Double.TryParse(stringArray[1], out tempDouble))
+                                    {
+                                        relationFactor = tempDouble;
+                                    }
+                                    break;
+                                case "DoYouWantTraitAffect":
+                                    if (int.TryParse(stringArray[1], out tempInteger))
+                                    {
+                                        if (tempInteger == 1)
+                                        {
+                                            doYouWantTraitAffect = true;
+                                        }
+                                    }
+                                    break;
+                                case "TraitLevelFactorMercy":
+                                    if (Double.TryParse(stringArray[1], out tempDouble))
+                                    {
+                                        traitLevelFactorMercy = tempDouble;
+                                    }
+                                    break;
+                                case "TraitLevelFactorHonor":
+                                    if (Double.TryParse(stringArray[1], out tempDouble))
+                                    {
+                                        traitLevelFactorHonor = tempDouble;
+                                    }
+                                    break;
+                                case "TraitLevelFactorGenerosity":
+                                    if (Double.TryParse(stringArray[1], out tempDouble))
+                                    {
+                                        traitLevelFactorGenerosity = tempDouble;
+                                    }
+                                    break;
+                                case "TraitLevelFactorCalculating":
+                                    if (Double.TryParse(stringArray[1], out tempDouble))
+                                    {
+                                        traitLevelFactorCalculating = tempDouble;
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public static void SetConfigurationDataToDefault()
+        {
+            doYouWantGlobalExecutionProbability = false;
+            globalExecutionProbability = 100;
+            doYouWantRelationFactor = false;
+            relationFactor = 0;
+            doYouWantTraitAffect = false;
+            traitLevelFactorMercy = 0;
+            traitLevelFactorHonor = 0;
+            traitLevelFactorGenerosity = 0;
+            traitLevelFactorCalculating = 0;
+        }
+    }
+}
